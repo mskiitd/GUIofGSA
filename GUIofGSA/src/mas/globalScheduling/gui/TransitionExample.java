9240ee4cb22ba5553a6aa1c206fc42dd7e9a76ad
nikhilbchilwant <nikhilbchilwant@gmail.com>
2015-03-28 15:53:37 +0530

    withput transition

--- GUIofGSA/src/mas/globalScheduling/gui/TransitionExample.java
+++ GUIofGSA/src/mas/globalScheduling/gui/TransitionExample.java
@@ -1,13 +1,11 @@
 package mas.globalScheduling.gui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.Icon;
@@ -20,20 +18,15 @@ import javax.swing.JPanel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.alee.extended.image.WebImage;
 import com.alee.extended.layout.FormLayout;
 import com.alee.extended.panel.GroupPanel;
-import com.alee.extended.transition.ComponentTransition;
-import com.alee.extended.transition.effects.curtain.CurtainTransitionEffect;
-import com.alee.extended.transition.effects.curtain.CurtainType;
-import com.alee.extended.window.TestFrame;
+import com.alee.laf.WebLookAndFeel;
 import com.alee.laf.button.WebButton;
 import com.alee.laf.label.WebLabel;
 import com.alee.laf.panel.WebPanel;
 import com.alee.laf.text.WebPasswordField;
 import com.alee.laf.text.WebTextField;
-import com.alee.utils.swing.WebTimer;
-
+//http://www.java-tips.org/index.php?option=com_content&task=view&sectionid=&id=1838&Itemid=61&mosmsg=Thanks+for+your+vote%21
 public class TransitionExample
 {
 	public static Logger log=LogManager.getLogger();
@@ -93,108 +86,44 @@ public class TransitionExample
 	    public static double width = screenSize.getWidth();
 	    public static double height = screenSize.getHeight();
 	    
-/*	    public static void main(String[] args) {
-	    	
-	    	
-	    	
+	    
+	    public static void main(String[] args) {
+	    	WebLookAndFeel.install();
 	        JFrame frame = new JFrame("Background Image Panel Example");
 
-	        // Create some GUI
-//	        MigLayout layout = new MigLayout("wrap 3");
-	        JPanel foregroundPanel = new JPanel();
-
+	        JPanel foregroundPanel = new JPanel(new BorderLayout(10, 10));
 	        foregroundPanel.setBorder(
-	                BorderFactory.createEmptyBorder(100,100,100,100));
+	                BorderFactory.createEmptyBorder(200,400,100,400));
 	        foregroundPanel.setOpaque(false);
 	        
-//	        foregroundPanel.add(new JLabel("Comment:"), BorderLayout.NORTH);
-//	        foregroundPanel.add(new JScrollPane(new JTextArea(5,30)),
-//	                BorderLayout.CENTER);
-//	        foregroundPanel.add(
-//	                new JLabel(
-//	                "Please enter your comments in text box above." +
-//	                " HTML syntax is allowed."), BorderLayout.SOUTH);
-	        WebPanel panel = new WebPanel ( new FormLayout ( false, true, 5, 5 ) );
+	        final WebPanel panel = new WebPanel ( new FormLayout ( false, true, 5, 40 ) );
+	        panel.setMargin(100,50, 100, 50);
 	        panel.add ( new WebLabel ( "Login:", WebLabel.TRAILING ) );
 	        panel.add ( new WebTextField ( "nikhil" ) );
 	        panel.add ( new WebLabel ( "Password:", WebLabel.TRAILING ) );
 	        panel.add ( new WebPasswordField ( "iitd" ) );
-//	        panel.setMargin(100, 100, 100, 100);
-	        panel.setBackground(Color.blue);
-//	        panel.setMaximumSize(new Dimension(1000, 1000));
-//	        panel.setMinimumSize(new Dimension(500, 500));
-	        foregroundPanel.add(panel, BorderLayout.CENTER);
+	        
+	        
+	        // Simple button
+	        WebButton okButton = new WebButton ( "Ok" );
+	        // Disabled button
+	        WebButton cancelButton = new WebButton ( "Cancel" );
+	        
+	        GroupPanel gp= new GroupPanel ( 10, new GroupPanel ( okButton ), cancelButton );
+	        panel.add(gp);
+	        
+	        foregroundPanel.add(panel,
+	                BorderLayout.CENTER);
 	        
 	        frame.setContentPane(wrapInBackgroundImage(foregroundPanel,
 	                new ImageIcon(
 	                "resources/LoginBackground.png")));
 	        
+	        frame.setExtendedState(Frame.MAXIMIZED_BOTH);
 	        frame.pack();
 	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	        frame.setVisible(true);
-	        
-	        
 	    }
-	}*/
-	    
-    public static void main ( final String[] args )
-    {
-
-    	ImageIcon IITDicon = new ImageIcon("resources/IITDlogo.png",
-                "");
-    	// First frame content
-        final WebImage image = new WebImage (IITDicon );
-
-    	/*ImageIcon loginBackgroundicon = new ImageIcon("resources/LoginBackground.png",
-                "");*/
-    	
-    	
-//    	final WebImage loginBackgroundimg = new WebImage (loginBackgroundicon );
-        // Second frame content
-    	
-    	
-        final WebPanel panel = new WebPanel ( new FormLayout ( false, true, 5, 5 ) );
-//        log.info(center_X+" "+panel.getHeight());
-//        panel.setBackground(Color.BLUE);
-        
-        
-        panel.setMargin(300, 500, 300, 500);
-        panel.add ( new WebLabel ( "Login:", WebLabel.TRAILING ) );
-        panel.add ( new WebTextField ( "nikhil" ) );
-        panel.add ( new WebLabel ( "Password:", WebLabel.TRAILING ) );
-        panel.add ( new WebPasswordField ( "iitd" ) );
-//        panel.add(loginBackgroundimg);
-        
-        
-        // Simple button
-        WebButton okButton = new WebButton ( "Ok" );
-        // Disabled button
-        WebButton cancelButton = new WebButton ( "Cancel" );
-        
-        GroupPanel gp= new GroupPanel ( 10, new GroupPanel ( okButton ), cancelButton );
-        panel.add(gp);
-        
-        // Transition panel and frame
-         final CurtainTransitionEffect effect = new CurtainTransitionEffect ();
-//         final SlideTransitionEffect effect = new SlideTransitionEffect ();
-//        final CurtainTransitionEffect effect = new CurtainTransitionEffect ();
-        effect.setSpeed ( 20 );
-        effect.setType(CurtainType.fade);
-        final ComponentTransition transition = new ComponentTransition ( image, effect );
-        TestFrame.show ( transition );
-        log.info(TestFrame.getFrames());
-        
-        // Transition action
-        WebTimer.delay ( 500, new ActionListener ()
-        {
-            @Override
-            public void actionPerformed ( final ActionEvent e )
-            {
-                transition.performTransition ( panel );
-            }
-        } );
-    }
-    }
-
+}
     
 
